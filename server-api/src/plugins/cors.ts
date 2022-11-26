bb838932bad1728748c4f1ea2ecfd2b6b3178bb9
Adrian Martinez <adriandev.me@gmail.com>
2022-11-26 12:40:05 +0100

    modificada la configuracion de cors, ahora lo habilita en funcion del parametro del fichero .env

--- server-api/src/plugins/cors.ts
+++ server-api/src/plugins/cors.ts
@@ -1,10 +1,13 @@
 import fp from "fastify-plugin";
 import cors, { FastifyCorsOptions } from "@fastify/cors";
 
+const corsEnabled: boolean =
+  process.env.SERVER_CORS_ENABLED?.toLowerCase() === "true";
+
 export default fp<FastifyCorsOptions>(async (fastify) => {
   const corsOptions: FastifyCorsOptions = {
-    // This is NOT recommended for production as it enables reflection exploits
-    origin: true,
+    // Activo desde el fichero .env
+    origin: corsEnabled,
   };
   fastify.register(cors, corsOptions);
 });
